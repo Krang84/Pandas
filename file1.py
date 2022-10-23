782922821210f434bd707a608dcca9ede488240d
Krang84 <patelassad@gmail.com>
2022-10-23 07:59:26 +0200

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,3 +1,15 @@
 import pandas as pd
-pd.read_table("file.txt")
+data={'state':['Ohio','Ohio','Ohio','Nevada','Nevada','Nevada'],
+      'year':[2000,2001,2002,2001,2002,2003],
+      'pop':[1.5,1.7,3.6,2.4,2.9,3.2]
+      }
+#print(data)
+frame = pd.DataFrame(data)
+#print(frame)
+
+frame2=pd.DataFrame(data, columns=["year", "state", "pop", "debt"], index=["one","two","three","four","five","six"])
+frame2["year"]
+frame2.loc["three"]
+
+frame2 = pd.Series([1,2], index=["five","six"])
 
