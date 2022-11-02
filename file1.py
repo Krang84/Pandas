b9e4a7fa1aa7381a052dd04e296cc6b7ed932d59
Krang84 <patelassad@gmail.com>
2022-11-02 19:00:41 +0100

    Update file1.py

--- file1.py
+++ file1.py
@@ -25,4 +25,7 @@ frame2["eastern"] = frame2["state"] == "Ohio"
 
 del frame2["eastern"]
 
+pop  = {'Nevada': {2001: 2.4, 2002:2.9}, 'Ohio': {2000:1.5,2001:1.7,2002:3.6}}
+
+frame3=pd.DataFrame(pop)
 
