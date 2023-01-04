74fea2641c46e31541293cf75fbd9c7a26e375f5
Krang84 <patelassad@gmail.com>
2023-01-04 20:54:22 +0100

    Update chap1.py

--- chap1.py
+++ chap1.py
@@ -14,3 +14,13 @@ frame = pd.DataFrame(my_dict)
 #    2         5
 #    3         6
 
+# learn
+# pd.DataFrame(dict)
+
+# Example 1
+
+data = {"state":["Ohio", "Ohio", "Ohio", "Nevada", "Nevada", "Nevada"],
+        "year":[2000,2001,2002,2001,2002,2003],
+        "pop":[1.5,1.7,3.6,2.4,2.9,3.2] }
+frame = pd.DataFrame(data)
+
